3ce384f97895f52908ed81eb09a8fb935a30744c
p-wn <93124806+p-wn@users.noreply.github.com>
2026-05-07 11:58:27 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const SKINS = [
       "/images/skins/wmii-result-beatoraja/02.webp",
       "/images/skins/wmii-result-beatoraja/03.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1LPM--2jgWpvfv-OkXTzHO-zErJDZAIVZ/view?usp=sharing",
+    downloadUrl: "https://drive.google.com/file/d/1_vp7aDaRn6lzIUoGKdWUYWuXw1HKbb3p/view?usp=sharing",
     date: "26.05.07",
   },
   {
@@ -25,7 +25,7 @@ const SKINS = [
       "/images/skins/wmii-play-beatoraja/01.webp",
       "/images/skins/wmii-play-beatoraja/02.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1qWpZdRdvhwnKg37C6bPRZLfE4VEEDW-Y/view?usp=sharing",
+    downloadUrl: "https://drive.google.com/file/d/1F-VXHyVoJanxSobCDat-QeoOvJJO13eC/view?usp=sharing",
     date: "26.05.07",
   },
   {
@@ -363,9 +363,13 @@ export default function App() {
                 <h3 className="text-sm font-black tracking-widest text-slate-900 uppercase">Update</h3>
               </div>
               <div className="space-y-3">
+                <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
+                  <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.07 - 2</span>
+                  <p className="text-sm text-slate-600 font-medium">WMII RESULT SKIN 게이지명이 ex-hard 단위까지 제대로 표시되도록 수정 / WMII PLAY SKIN Betoraja용 게이지명이 ex-hard단위까지 표시되도록 수정</p>
+                </div>  
                  <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
                   <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.07</span>
-                  <p className="text-sm text-slate-600 font-medium">WMII RESULT SKIN 단위인정에서 게이지명이 a-easy로 뜨는것 (ex)class gauge로 뜨게 변경, misscount가 +1이상일때만 빨간색으로 뜨도록 변경, 랜덤 패턴이 버튼색도 바뀌도록 변경 / WMII PLAY SKIN Betoraja용 단위에서 게이지명이 class gauge로 뜨도록 수정, BEGINNER 오타수정(lr2fhd버전 공통)</p>
+                  <p className="text-sm text-slate-600 font-medium">WMII RESULT SKIN 단위인정에서 게이지명이 a-easy로 뜨는것 class gauge로 뜨게 변경, misscount가 +1이상일때만 빨간색으로 뜨도록 변경, 랜덤 패턴이 버튼색도 바뀌도록 변경 1st~final표기 추가/ WMII PLAY SKIN Betoraja용 단위에서 게이지명이 class gauge로 뜨도록 수정, BEGINNER 오타수정(lr2fhd버전 공통)</p>
                 </div>  
                 <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
                   <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.06</span>
