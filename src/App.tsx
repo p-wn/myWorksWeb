f3767e1118b2f3a636f429544ea5652e0603a79e
p-wn <pawn0103@gmail.com>
2026-05-21 00:28:14 +0900

    asdasdasdasdasd

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ const SKINS = [
       "/images/skins/wmii-result-beatoraja/03.webp",
       "/images/skins/wmii-result-beatoraja/04.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1INRVF-BL1Zh6yJYjtyQqOnI9OBzWfeZs/view?usp=sharing",
-    date: "26.05.17",
+    downloadUrl: "https://drive.google.com/file/d/19DiwxkLf5pnGSizg-0CSc8DDtBP4NPcN/view?usp=sharing",
+    date: "26.05.21",
   },
   {
     id: 1,
@@ -364,6 +364,10 @@ export default function App() {
                 <h3 className="text-sm font-black tracking-widest text-slate-900 uppercase">Update</h3>
               </div>
               <div className="space-y-3">
+                       <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
+                  <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.21</span>
+                  <p className="text-sm text-slate-600 font-medium">WMII RESULT SKIN 최초 플레이시 미스카운트부분 공백에 막대가 뜨도록 변경, max시 max+0000표시되도록 수정, 코스리절트 작은숫자관련 칸 크기 수정</p>
+                </div> 
                  <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
                   <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.20</span>
                   <p className="text-sm text-slate-600 font-medium">WMII PLAY SKIN 새로운 Bomb 파츠 추가</p>
