324ef7d26f22fd4896b06b3748aaefb1ebda8e8a
p-wn <pawn0103@gmail.com>
2026-05-22 01:43:40 +0900

    desc

--- src/App.tsx
+++ src/App.tsx
@@ -366,7 +366,7 @@ export default function App() {
               <div className="space-y-3">
                                 <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
                   <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.22</span>
-                  <p className="text-sm text-slate-600 font-medium">WMII PLAY SKIN에 Bomb 파츠 shock, twirl추가</p>
+                  <p className="text-sm text-slate-600 font-medium">WMII PLAY SKIN에 새로운 custom pats로 Bomb종류 shock, twirl추가</p>
                 </div> 
                        <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
                   <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2 py-0.5 rounded w-fit">26.05.21</span>
