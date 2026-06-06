89a75a7478b4d76583f8bca541aac27520b09e52
p-wn <pawn0103@gmail.com>
2026-06-07 00:56:10 +0900

    ir name

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ const SKINS = [
       "/images/skins/wmii-result-beatoraja/03.webp",
       "/images/skins/wmii-result-beatoraja/04.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1804nPzhskd8gfj2TMxDH2RRB04ZKvUan/view?usp=sharing",
-    date: "26.05.29",
+    downloadUrl: "https://drive.google.com/file/d/13X4UY2L-9DISG2pRdW-ueCQxVf4qwZjy/view?usp=sharing",
+    date: "26.06.07",
   },
   {
     id: 2,
@@ -352,7 +352,13 @@ export default function App() {
               
               <div className="space-y-4 max-h-[260px] overflow-y-auto pr-2 scrollbar-thin scrollbar-thumb-slate-200 scrollbar-track-transparent">
                 <div className="relative border-l-2 border-slate-100 pl-4 space-y-6 py-1">
-                  
+                          <div className="relative group">
+                    <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
+                    <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
+                      <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2.5 py-1 rounded-lg w-fit">26.06.07</span>
+                      <p className="text-sm text-slate-700 font-medium">WMII RESULT SKIN 인터넷 랭킹에서 "YOU" 대신 자기 이름 표시 기능 추가 </p>
+                    </div> 
+                  </div>
                   <div className="relative group">
                     <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
                     <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
