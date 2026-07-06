2c077744f83a10f17e23a5cffd072dadf72ac459
p-wn <pawn0103@gmail.com>
2026-07-06 22:23:33 +0900

    asdasdasdasd

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ const SKINS = [
       "/images/skins/wmii-result-beatoraja/03.webp",
       "/images/skins/wmii-result-beatoraja/04.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1D6LeL_TGcaEgrxv3iJ-iw4yNTPehJjoX/view?usp=sharing",
-    date: "26.06.23",
+    downloadUrl: "https://drive.google.com/file/d/1TZ8OM0Cg-X2Wd41Z65Mqxdldu0vUFgXT/view?usp=sharing",
+    date: "26.07.06",
   },
   {
     id: 2,
@@ -26,8 +26,8 @@ const SKINS = [
       "/images/skins/wmii-play-beatoraja/01.webp",
       "/images/skins/wmii-play-beatoraja/02.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1gdE7-EclO4yL4ncXXl2ktJezGaJAe3vD/view?usp=sharing",
-    date: "26.06.26",
+    downloadUrl: "https://drive.google.com/file/d/1GnRvtwlC9cUub-CruniVcO3qpLmglj5n/view?usp=sharing",
+    date: "26.07.06",
   },
   {
     id: 3,
@@ -442,7 +442,14 @@ export default function App() {
                 </div>
               </div>
               <div className="space-y-4 max-h-[260px] overflow-y-auto pr-2 scrollbar-thin scrollbar-thumb-slate-200 scrollbar-track-transparent">
-                <div className="relative border-l-2 border-slate-100 pl-4 space-y-6 py-1">          
+                <div className="relative border-l-2 border-slate-100 pl-4 space-y-6 py-1">      
+                      <div className="relative group">
+                    <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
+                    <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
+                      <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2.5 py-1 rounded-lg w-fit">26.07.06</span>
+                      <p className="text-sm text-slate-700 font-medium">WMII PLAY SKIN AC스킨 변속곡 green number 위치 수정 / WMII RESULT SKIN 다음랭크와의 차이점수 계산식 수정 </p>
+                    </div> 
+                  </div>    
                                       <div className="relative group">
                     <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
                     <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
