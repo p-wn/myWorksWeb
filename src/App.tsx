271e2cc7913e689a741550d3f121eeafb34109fb
p-wn <pawn0103@gmail.com>
2026-08-13 02:12:55 +0900

    sdfsdfsdf

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const SKINS = [
       "/images/skins/wmii-play-beatoraja/03.webp",
       "/images/skins/wmii-play-beatoraja/04.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1UtIrRU9kfxWRdL-ghRnqXOofJRUuKvjc/view?usp=sharing",
+    downloadUrl: "https://drive.google.com/file/d/1v7ExTDZVEhReJw_bbVgbm1jHdN5t-KWP/view?usp=sharing",
     date: "26.08.13",
   },
   {
@@ -450,7 +450,7 @@ export default function App() {
                     <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
                     <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
                       <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2.5 py-1 rounded-lg w-fit">26.08.13</span>
-                      <p className="text-sm text-slate-700 font-medium">WMII PLAY SKIN for Beatoraja의 Bmz player 호환문제 수정</p>
+                      <p className="text-sm text-slate-700 font-medium">WMII PLAY SKIN for Beatoraja의 Bmz player 호환문제 수정, AC 14key practice 모드 문제 수정</p>
                     </div> 
                   </div>  
                             <div className="relative group">
