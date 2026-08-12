a2e2cd2e4f1d1fcda6f3307ec4ce8928666d8715
p-wn <pawn0103@gmail.com>
2026-08-13 01:42:26 +0900

    sdfsdf

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ const SKINS = [
       "/images/skins/wmii-play-beatoraja/03.webp",
       "/images/skins/wmii-play-beatoraja/04.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/1DP70G1X08vdc3dvle8Cj0HI93nRs-K6e/view?usp=sharing",
-    date: "26.08.09",
+    downloadUrl: "https://drive.google.com/file/d/1UtIrRU9kfxWRdL-ghRnqXOofJRUuKvjc/view?usp=sharing",
+    date: "26.08.13",
   },
   {
     id: 3,
@@ -445,7 +445,14 @@ export default function App() {
                 </div>
               </div>
               <div className="space-y-4 max-h-[260px] overflow-y-auto pr-2 scrollbar-thin scrollbar-thumb-slate-200 scrollbar-track-transparent">
-                <div className="relative border-l-2 border-slate-100 pl-4 space-y-6 py-1"> 
+                <div className="relative border-l-2 border-slate-100 pl-4 space-y-6 py-1">        
+                   <div className="relative group">
+                    <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
+                    <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
+                      <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2.5 py-1 rounded-lg w-fit">26.08.13</span>
+                      <p className="text-sm text-slate-700 font-medium">WMII PLAY SKIN for Beatoraja의 Bmz player 호환문제 수정</p>
+                    </div> 
+                  </div>  
                             <div className="relative group">
                     <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
                     <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
