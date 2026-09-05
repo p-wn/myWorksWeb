2939dcaabecb684f4ccebcbf74aed2877890c092
p-wn <pawn0103@gmail.com>
2026-09-06 00:58:37 +0900

    ffffff

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ const SKINS = [
       "/images/skins/wmii-play-beatoraja/03.webp",
       "/images/skins/wmii-play-beatoraja/04.webp",
     ],
-    downloadUrl: "https://drive.google.com/file/d/12yRQpoRiL3Ps8inY3EZN6cyXL5cG8VPc/view?usp=sharingg",
-    date: "26.09.03",
+    downloadUrl: "https://drive.google.com/file/d/1sI6MONAlvH5MMhGRmsRn96w_nfBg4bzb/view?usp=sharing",
+    date: "26.09.06",
   },
   {
     id: 3,
@@ -446,11 +446,11 @@ export default function App() {
               </div>
               <div className="space-y-4 max-h-[260px] overflow-y-auto pr-2 scrollbar-thin scrollbar-thumb-slate-200 scrollbar-track-transparent">
                 <div className="relative border-l-2 border-slate-100 pl-4 space-y-6 py-1"> 
-                              <div className="relative group">
+                                                <div className="relative group">
                     <div className="absolute -left-[21px] top-1.5 w-2 h-2 rounded-full bg-point ring-4 ring-white transition-all group-hover:scale-125" />
                     <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
-                      <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2.5 py-1 rounded-lg w-fit">26.09.03</span>
-                      <p className="text-sm text-slate-700 font-medium">WMII PLAY SKIN for Beatoraja 다음 랭크까지 점수 ref 변경(bmz 0.4.0 대비)</p>
+                      <span className="text-xs font-mono font-bold text-point bg-teal-50 px-2.5 py-1 rounded-lg w-fit">26.09.06</span>
+                      <p className="text-sm text-slate-700 font-medium">WMII PLAY SKIN for Beatoraja GHOST 숫자 색상 흰색/빨간색 또는 흰색으로만 표시하는 옵션 추가(다음랭크까지 점수도 같이 변함)</p>
                     </div> 
                   </div>
                         <div className="relative group">
